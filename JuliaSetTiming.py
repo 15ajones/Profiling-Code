c76b94439b797c316cf9ce730825ce772df89922
15ajones <15ajones0201@gmail.com>
2024-01-31 12:56:01 +0100

    ready to submit

--- JuliaSetTiming.py
+++ JuliaSetTiming.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     # Calculate the Julia set using a pure Python solution with
     # reasonable defaults for a laptop
     
-    calc_pure_python(desired_width=100, max_iterations=30)
+    calc_pure_python(desired_width=1000, max_iterations=30)
     
     
 # -------------------------------------------------------------------------------
